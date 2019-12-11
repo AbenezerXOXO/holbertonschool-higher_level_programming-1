b47c0319866366fdaff5b7e73213c38e390bfbeb
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-12-11 05:55:30 -0800

    adv c

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
+    copy = ""
+    for i in range(0, len(str)):
         if i != n:
-            copy[i] = str[i]
-        else:
-            i = i + 1
+            copy += str[i]
+    return copy
