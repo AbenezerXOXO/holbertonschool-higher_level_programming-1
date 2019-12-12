9ec5e064588fa5ddc52b4561dad2da722c0f46c6
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-12-12 09:44:08 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     elif len(argv) == 2:
         print("1 argument:\n1: {}".format(argv[1]))
     else:
-        print("{:d}: arguments:".format(len(argv) - 1))
+        print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{:d}: {}".format(i, argv[i]))
