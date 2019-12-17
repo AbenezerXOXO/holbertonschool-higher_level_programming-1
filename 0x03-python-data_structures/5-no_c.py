9ef1191d001c71c5ae81f85d380a28c3389eeb02
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-12-17 04:05:54 -0800

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
+        copy = ""
         for i in range(0, len(my_string)):
-            if my_string[i] == 'c' or my_string[i] == 'C':
-                my_string[i] = ""
-    return my_string
+            if my_string[i] != 'c' and my_string[i] != 'C':
+                copy += my_string[i]
+    return copy
