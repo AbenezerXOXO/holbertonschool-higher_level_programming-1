28562d4f90f2b48df6a5ff97d1dcf88660a8dd3e
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-12-17 04:02:09 -0800

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if my_list:
         copy = []
-        copy = my_list
+        copy = my_list[:]
         if idx < 0 or idx >= len(my_list):
             return copy
         else:
