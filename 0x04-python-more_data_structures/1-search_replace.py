914a1f20b42e2b213b79c7b6aa94df7c4a72a5a2
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-12-19 08:38:32 -0800

    change search_replace function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for elem in my_list:
-        if elem == search:
-            new_list.append(replace)
-        else:
-            new_list.append(elem)
+    new_list = [x if x != search else replace for x in my_list]
     return new_list
