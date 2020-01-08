ac575805dfca4dc5bd69e976222331039950ca8e
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-08 20:38:08 +0100

    check value error + type error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,9 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
+    except TypeError as te:
+        print("Exception:", te, file=sys.stderr)
+        return False
     except ValueError as err:
         print("Exception:", err, file=sys.stderr)
         return False
