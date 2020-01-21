6366cc11d74f17af750b8663ef76c15891441368
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-21 23:57:04 +0100

    fix everything

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ def add_attribute(obj, name, value):
     """
     adds a new attribute to an object if it’s possible
     """
-    if type(obj) in [str, int, bool, set, tuple, list]:
+    if type(obj) in [str, int, float, complex, bool, set, tuple, list]:
         raise TypeError("can't add new attribute")
     else:
         obj.name = value
