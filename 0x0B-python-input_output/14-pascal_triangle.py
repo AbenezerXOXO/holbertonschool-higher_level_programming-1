fdf3886cc9ff7d2393505a0981d3ce52c65e3bc7
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-22 23:51:00 +0100

    unfinished tasks 13 and 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains one fucntion
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    big = []
+    for i in range(0, n):
+        small = []
+
+        big.append(small)
+    return big
