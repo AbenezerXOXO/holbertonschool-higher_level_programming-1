c9aa2905e371e4a6853371868ee5a24070dee8b5
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-22 23:15:21 +0100

    fix trailing space

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -6,7 +6,7 @@ import json
 
 
 def class_to_json(obj):
-    """ 
+    """
     returns the dictionary description
     with simple data structure
     (list, dictionary, string, integer and boolean)
