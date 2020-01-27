b71681a95d501d233defefc94b453da06c748592
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-27 22:44:05 +0100

    first 8 tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains one class: Base
+"""
+
+
+class Base:
+    """
+    The goal of this class is:
+    to manage id attribute in all our future classes
+    and to avoid duplicating the same code
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ instantiation """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
