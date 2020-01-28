ae9635fb8e4cd85146bacb38d0ae3eb6e26903f5
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-28 23:07:42 +0100

    unittest for Square and Rectangle class

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -34,11 +34,14 @@ class testclass(unittest.TestCase):
         with self.assertRaises(AttributeError):
             print(r1.__y)
 
-    def test_missing_arguments(self):
+    def test_wrong_arguments_number(self):
+        """ checks the number of arguments """
         with self.assertRaises(TypeError):
             r3 = Rectangle()
         with self.assertRaises(TypeError):
             r3 = Rectangle(1)
+        with self.assertRaises(TypeError):
+            r3 = Rectangle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 
     def test_validation(self):
         """
@@ -69,3 +72,32 @@ class testclass(unittest.TestCase):
         """ test area method """
         r3 = Rectangle(5, 5)
         self.assertEqual(r3.area(), 25)
+
+    def test_str(self):
+        """ test str method """
+        r3 = Rectangle(4, 6, 2, 1, 12)
+        r4 = Rectangle(4, 4)
+        s = str(r3)
+        s2 = str(r4)
+        self.assertEqual(s, "[Rectangle] (12) 2/1 - 4/6")
+        self.assertEqual(s2, "[Rectangle] (6) 0/0 - 4/4")
+
+    def test_update(self):
+        """ test update method """
+        r3 = Rectangle(5, 5, 5, 5, 5)
+        r3.update(1)
+        s = str(r3)
+        self.assertEqual(s, "[Rectangle] (1) 5/5 - 5/5")
+        r3.update(1, 1, 1, 1, 1)
+        s = str(r3)
+        self.assertEqual(s, "[Rectangle] (1) 1/1 - 1/1")
+
+    def test_updatek(self):
+        """ test update method (*args and **kwargs) """
+        r3 = Rectangle(5, 5, 5, 5, 5)
+        r3.update(1, height=1)
+        s = str(r3)
+        self.assertEqual(s, "[Rectangle] (1) 5/5 - 5/5")
+        r3.update(height=1, x=1, y=1, id=1, width=1)
+        s = str(r3)
+        self.assertEqual(s, "[Rectangle] (1) 1/1 - 1/1")
