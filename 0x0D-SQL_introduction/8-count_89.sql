7a4c63cf47d40f8eba3da089ef878a794ec601ca
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-03-02 21:19:27 +0100

    create table and insert rows

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- a script that displays the number of records with id = 89
+-- in the table first_table in the current database in the MySQL server
+SELECT count( * ) as total_record FROM first_table where id = 89
