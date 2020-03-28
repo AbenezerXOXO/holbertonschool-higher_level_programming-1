a83e52059d086b9782f9279a123b1a3a57b4c772
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-03-28 19:44:49 +0100

    adding BINARY option to make WHERE LIKE case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,13 +12,11 @@ def main(argv):
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    try:
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-    except:
-        pass
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    pass
     cur.close()
     conn.close()
 
