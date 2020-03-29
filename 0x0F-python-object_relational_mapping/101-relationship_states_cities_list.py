534553b245b366acd54345e37e1185b7b1985c75
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-03-30 00:18:50 +0100

    second advanced task

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ a script that all State objects, and corresponding City objects
 from the database hbtn_0e_100_usa """
-import sys
+from sys import argv
 from relationship_state import Base, State, City
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
@@ -21,5 +21,5 @@ def main(argv):
     session.close()
 
 
-if __name__ == '__main__' and len(sys.argv) == 4:
-    main(sys.argv)
+if __name__ == '__main__' and len(argv) == 4:
+    main(argv)
