00f02048222297ae20f21e5059e9c0f9e2674918
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-04-13 20:33:06 +0100

    task: catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# undone task
-curl -s -I -X OPTIONS 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me
+curl -s -L -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
