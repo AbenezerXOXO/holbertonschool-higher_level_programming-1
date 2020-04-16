f044d5f6fcce5c2690506d37873933b9d0788dad
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-04-16 20:16:34 +0100

    get header value in urllib python

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == '__main__':
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        print(response.info()['X-Request-Id'])
