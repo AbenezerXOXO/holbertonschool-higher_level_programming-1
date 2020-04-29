6b8476cedb6f82e4e5308167283509a12f7f464e
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-04-29 22:48:49 +0100

    put back first code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,9 @@ if __name__ == '__main__':
     url = 'https://api.github.com/repos/' + sys.argv[1] + '/' + sys.argv[2] + \
           '/commits'
     r = requests.get(url)
-    s = r.json()
-    for i in range(10):
-        print("{}: {}".format(s[i]['sha'],
-                              s[i]['commit']['author']['name']))
+    try:
+        for i in range(10):
+            print("{}: {}".format(r.json()[i]['sha'],
+                                  r.json()[i]['commit']['author']['name']))
+    except:
+        pass
