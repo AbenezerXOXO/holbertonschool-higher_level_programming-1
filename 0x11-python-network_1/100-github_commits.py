5c6af28fa362aa9fa9e91aadf95612465d82d264
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-04-29 20:38:13 +0100

    display commits using girhub api

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a Python script that uses github api to list 10 commits
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/' + sys.argv[1] + '/' + sys.argv[2] + '/commits'
+    r = requests.get(url)
+    try:
+        for i in range(10):
+            print("{}: {}".format(r.json()[i]['sha'], r.json()[i]['commit']['author']['name']))
+    except:
+        pass
