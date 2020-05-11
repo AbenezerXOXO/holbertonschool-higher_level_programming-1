63a184752f98b9399dd58165d2f5c2f07840d4a3
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-11 20:53:36 +0100

    decrement

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const rev = [];
   let i;
-  for (i = list.length - 1; i >= 0; i++) {
+  for (i = list.length - 1; i >= 0; i--) {
     rev.push(list[i]);
   }
   return rev;
