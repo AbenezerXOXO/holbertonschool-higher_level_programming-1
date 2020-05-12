f2fb1469f2b4504f9c85ff8ed759cfc574d1a4a5
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-12 15:36:40 +0100

    fix output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
   if (error) throw error;
-  else console.log(response.statusCode);
+  else console.log('code:', response.statusCode);
 });
