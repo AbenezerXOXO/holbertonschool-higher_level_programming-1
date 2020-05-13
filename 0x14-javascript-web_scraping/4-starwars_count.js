50f24a64ef23800b04c4f99e3e20693db5abde7c
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-13 09:25:02 +0100

    fix task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,9 @@ request.get(process.argv[2], function (error, response, body) {
   else if (response.statusCode === 200) {
     let c = 0;
     const results = JSON.parse(body).results;
-    const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+    const wedge = '/api/people/18/';
     for (let i = 0; i < results.length; i++) {
-      if (results[i].characters.includes(wedge)) {
+      if (results[i].characters.find(el => el.includes(wedge))) {
         c++;
       }
     }
